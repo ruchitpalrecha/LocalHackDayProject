169a2bf11749bcc1d066679e09b249a6a0e2fd9d
ruchitp <ruchitpalrecha@hotmail.com>
2019-11-30 14:15:02 -0800

    Reconfigured Project

---
+++ BorderControl/ViewControllers/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  BorderControl
+//
+//  Created by Ruchit Palrecha on 11/30/19.
+//  Copyright © 2019 ScholarSquad. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
