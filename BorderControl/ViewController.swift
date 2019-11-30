c8ec33f997f57db900a5a3af587fe98f0be32963
ruchitp <ruchitpalrecha@hotmail.com>
2019-11-30 12:40:41 -0800

    Created locationService checking

--- BorderControl/ViewController.swift
+++ BorderControl/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        checkLocationServices()
+    }
+    
+    func checkLocationServices() {
+      if (CLLocationManager.locationServicesEnabled()) {
+        checkLocationAuthorization()
+      } else {
+        // Show alert letting the user know they have to turn this on.
+      }
+    }
+    
+    func checkLocationAuthorization() {
+        print("Got to checking location");
+        switch (CLLocationManager.authorizationStatus()) {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+        case .authorizedAlways:
+            mapView.showsUserLocation = true
+            break
+        case .denied: // Show alert telling users how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
+        case .restricted: // Show an alert letting them know what’s up
+            break
+        }
     }
 
 
